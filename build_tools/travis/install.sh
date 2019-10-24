518f726ac447b0635aaa277f90a335cc8f43e50c
alfaro96 <alfaro96@alfaro96.local>
2019-10-24 22:55:23 +0200

    [MAINT] Build Travis using NumPy, Scipy and Cython developer versions.

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -62,13 +62,10 @@ source activate testenv
 echo "Installing setuptools"
 pip install --upgrade pip setuptools
 
-# NumPy and SciPy
-echo "Installing NumPy and SciPy"
-pip install --upgrade numpy scipy
-
-# Cython
-echo "Installing Cython"
-pip install --upgrade cython
+# NumPy, SciPy and Cython
+echo "Installing NumPy, SciPy and Cython"
+dev_url=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com
+pip install --pre --upgrade --timeout=60 -f $dev_url numpy scipy cython
 
 # Pytest
 echo "Installing pytest"
