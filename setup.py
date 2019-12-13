e7169285c4939c6975c232ca36d6577811ac6402
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2019-12-13 22:53:22 +0100

    [FIX] Remove compatibility with Python 3.8.

--- setup.py
+++ setup.py
@@ -63,7 +63,6 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: Implementation :: CPython",
     "Topic :: Scientific/Engineering :: Artificial Intelligence"
 ]
