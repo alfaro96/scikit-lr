57e6def6a46eb7b12c7f31da40272178b1a6f91d
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-01-25 17:14:42 +0100

    [MNT] Review _build_utils module.

--- sklr/_build_utils/__init__.py
+++ sklr/_build_utils/__init__.py
@@ -28,91 +28,92 @@ CYTHON_MIN_VERSION = "0.29.14"
 # =============================================================================
 
 def build_from_c_and_cpp_files(extensions):
-    """Modify the extensions to build from the .c and .cpp files.
-
-    This is useful for releases. This way, Cython is not required to
-    run python setup.py install.
-    """
-    # Obtain the source files when they
-    # correspond to a Cython extension module
+    """Modify the extensions to build
+    from the .c and .cpp files."""
+    # Modify the extensions to build from .c
+    # and .cpp files instead of .pyx or .py
     for extension in extensions:
-        # Initialize the source files from
-        # the current extension module
+        # Initialize the list of modified
+        # source files for this extension
         sources = []
-        # Check whether the source files
-        # correspond to a Cython module
-        for source_file in extension.sources:
-            # Obtain the path to the source
-            # file and its extension
-            (path, ext) = splitext(source_file)
-            # If there exist a .pyx file or .py,
-            # obtain the .c or .cpp extension
+        # Modify the source files of this extension
+        # (according to the previous criteria)
+        for source in extension.sources:
+            # Obtain the path to this source
+            # file and the file extension
+            (path, ext) = splitext(source)
+            # Extract the .c or .cpp extension
+            # of this .pyx or .py source file
             if ext in {".pyx", ".py"}:
                 if extension.language == "c++":
                     ext = ".cpp"
                 else:
                     ext = ".c"
-                # Replace the current source
-                # file by the .c or .cpp one
-                source_file = path + ext
-            # Append the source file to
-            # the list of source files
-            sources.append(source_file)
-        # Add them to the extension module
+                # Replace the extension
+                # of this source file
+                source = path + ext
+            # Append the modified source
+            # file to the list of modified
+            # source files of this extension
+            sources.append(source)
+        # Add the modified source
+        # files to this extension
         extension.sources = sources
 
 
 def get_cython_version():
-    """Return a string containing the Cython version
-    string (empty string if not installed)."""
+    """Return a string containing the Cython
+    version (empty string if not installed)."""
     # Try to import Cython and, if it is not
-    # found, show the error message to the user
+    # found, show an error message to the user
     try:
         # Import
         import Cython
-        # Get the current version
+        # Get the installed version
         cython_version = Cython.__version__
     except ImportError:
         # Show the error message
         print_exc()
-        # Set the version as an empty string
+        # Set the version to an empty string
         cython_version = ""
 
-    # Return the installed version
+    # Return installed
+    # version of Cython
     return cython_version
 
 
 def maybe_cythonize_extensions(top_path, config):
     """Tweaks for building extensions between release and development mode."""
-    # Check whether the current package is a release or development
-    # looking for the existance of the "PKG-INFO" file
-    # (only available in release mode)
+    # Check whether the package is in release or development
+    # looking to the existance of the "PKG-INFO" file
+    # (which is only available in release mode)
     is_release = exists(join(top_path, "PKG-INFO"))
 
-    # If it is a release, obtain the extensions modules
-    # from the corresponding .c and .cpp files
+    # If it is a release, directly build the
+    # extensions from the .c and .cpp source files
     if is_release:
         build_from_c_and_cpp_files(config.ext_modules)
-    # Otherwise, "cythonize" the .pyx files
+    # Otherwise, "cythonize" the .pyx or .py source files
     else:
-        # Get the Cython version needed to
-        # "cythonize" the extension modules
+        # Get the installed version of Cython
+        # (needed to "cythonize" the extensions)
         cython_version = get_cython_version()
         # If it is not installed, show the
-        # corresponding message to the user
+        # proper error message to the user
         if not cython_version:
             raise ImportError("Cython is not installed. "
                               "At least version {} is required."
                               .format(CYTHON_MIN_VERSION))
-        # Otherwise, if the version is not the required one, show the
-        # got and the required version with the corresponding message
+        # Otherwise, if the installed version is not the minimum required one,
+        # inform to the user about the got and the minimum required version
         elif parse_version(cython_version) < parse_version(CYTHON_MIN_VERSION):
             raise ImportError("Your installation of Cython is not "
                               "the required. Got {} but requires >={}."
                               .format(cython_version, CYTHON_MIN_VERSION))
 
-        # Import the required "cythonize" method
+        # Import the "cythonize" method
         from Cython.Build import cythonize
 
-        # "Cythonize" the extension modules
+        # "Cythonize" the extension modules from
+        # the corresponding .pyx and .py source files
         config.ext_modules = cythonize(config.ext_modules)
