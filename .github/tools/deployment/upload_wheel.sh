6ec1e2f91628f0e060d8b3dd9b16fed3050ed08f
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-05-20 10:53:07 +0200

    [MNT] Final review.

--- .github/tools/deployment/upload_wheel.sh
+++ .github/tools/deployment/upload_wheel.sh
@@ -2,6 +2,7 @@
 
 conda install -q -y anaconda-client
 
-# Force a replacement if the remote file already exists
 ls wheelhouse/*.whl
+
+# Force a replacement if the remote file already exists
 anaconda -t $CONDA_TOKEN upload --force -u alfaro96 wheelhouse/*.whl
