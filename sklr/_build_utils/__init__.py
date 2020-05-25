ffc0cf27ad62ecf3931cbd5dc610126d3a47a130
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-05-25 16:23:40 +0200

    [MNT] Add lgtm.yml and pyproject.toml files (#62).

--- sklr/_build_utils/__init__.py
+++ sklr/_build_utils/__init__.py
@@ -6,12 +6,13 @@
 # =============================================================================
 
 # Standard
-from distutils.version import LooseVersion
-from glob import glob
+from distutils.core import Extension
+import glob
 import os
 import sys
 
 # Third party
+from Cython.Build import cythonize
 import numpy as np
 
 
@@ -19,14 +20,7 @@ import numpy as np
 # Constants
 # =============================================================================
 
-# The following places must be sync
-# with regard to the Cython version:
-#   - .github/workflows/integration.yml
-#   - docker/requirements/build.txt
-#   - sklr/_build_utils/__init__.py
-CYTHON_MIN_VERSION = "0.29.14"
-
-# Extension modules that need to compile against NumPy should locate
+# The extension modules that need to compile against NumPy should locate
 # the corresponding include directory (source directory or core headers)
 if np.show_config is None:
     d = os.path.join(os.path.dirname(np.__file__), "core", "include")
@@ -35,95 +29,42 @@ else:
 
 NUMPY_HEADERS_PATH = d
 
-# Set the NumPy C API to a specific version instead of NPY_API_VERSION
-# to avoid that the compilation is broken for released versions when
-# NumPy introduces a new deprecation
-NUMPY_NODEPR_API = dict(define_macros=[("NPY_NO_DEPRECATED_API",
-                                        "NPY_1_9_API_VERSION")])
-
 
 # =============================================================================
 # Methods
 # =============================================================================
 
-def make_config(config):
-    """Make the configuration of a module."""
-    # Replace the point by a slash to list all the
-    # directories and files in the current module
-    path = config.name.replace(".", "/")
-    (_, dirs, files) = next(os.walk(path))
+def create_extension(extension_path):
+    """Create and return an extension module."""
+    # Replace the slash by a dot to get the extension name
+    (extension_name, _) = os.path.splitext(extension_path)
+    extension_name = extension_name.replace("/", ".")
 
-    # Find the C++ source files available in the current module, since
-    # they must be included along with (some of) the Cython extensions
-    cpp_files = glob(os.path.join(path, "src", "**/*.cpp"), recursive=True)
-    cpp_files = [cpp_file.replace(path + "/", "") for cpp_file in cpp_files]
-
-    for module_dir in dirs:
-        if module_dir == "data":
-            config.add_data_dir(module_dir)
-        elif module_dir != "src":
-            config.add_subpackage(module_dir)
-
-    for module_file in files:
-        if module_file.endswith(".pxd"):
-            config.add_data_files(module_file)
-        elif module_file.endswith(".pyx"):
-            add_extension(config, module_file, *cpp_files)
-
-    # Skip cythonization as it is not wanted to include the
-    # generated C/C++ files in the release tarballs as they
-    # are not necessarily, just for forward compatible with
-    # future versions of Python, for instance
-    if "sdist" not in sys.argv and config.name == "sklr":
-        cythonize_extensions(config.top_path, config)
-
-    return config
-
-
-def add_extension(config, *sources):
-    """Add an extension to the configuration."""
-    (name, _) = os.path.splitext(sources[0])
-
+    extension_path = [extension_path]
     include_dirs = [NUMPY_HEADERS_PATH]
 
-    # Specify the C++ version and ISO standard
-    # for accessing to advanced features when
-    # building the package for manylinux systems
     extra_link_args = ["-std=c++11"]
     extra_compile_args = ["-O3", "-std=c++11"]
 
-    config.add_extension(name, sources,
-                         language="c++",
-                         include_dirs=include_dirs,
-                         extra_link_args=extra_link_args,
-                         extra_compile_args=extra_compile_args,
-                         **NUMPY_NODEPR_API)
+    return Extension(extension_name,
+                     extension_path,
+                     language="c++",
+                     include_dirs=include_dirs,
+                     extra_link_args=extra_link_args,
+                     extra_compile_args=extra_compile_args)
 
 
-def _check_cython_version(min_version):
-    """Check that the version of Cython installed in the system
-    is greater than or equal the minimum required version."""
-    # Re-raise with a more informative message when Cython is not installed
-    try:
-        import Cython
-    except ModuleNotFoundError:
-        raise ModuleNotFoundError("Please install Cython with a version "
-                                  ">= {0} in order to build scikit-lr from "
-                                  "source.".format(min_version))
+def cythonize_extensions(module_name):
+    """Find and cythonize the extension modules."""
+    # Skip cythonization in the release tarballs since
+    # the generated C++ source files are not necessary
+    if "sdist" not in sys.argv:
+        pattern = os.path.join(module_name, "**/*.pyx")
+        extensions_paths = glob.glob(pattern, recursive=True)
 
-    if LooseVersion(Cython.__version__) < min_version:
-        raise ValueError("The current version of Cython is {0} installed "
-                         "in {1}.".format(Cython.__version__, Cython.__path__))
+        for (extension, extension_path) in enumerate(extensions_paths):
+            extensions_paths[extension] = create_extension(extension_path)
 
+        extensions = cythonize(extensions_paths)
 
-def cythonize_extensions(top_path, config):
-    """Check that a recent version of Cython is
-    available and cythonize the extension modules."""
-    _check_cython_version(CYTHON_MIN_VERSION)
-
-    # The cythonize method must be imported after checking
-    # the Cython version so that a more informative message
-    # can be provided to the user when Cython is not installed
-    from Cython.Build import cythonize
-
-    config.ext_modules = cythonize(config.ext_modules)
+        return extensions
