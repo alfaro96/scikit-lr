85e1397cbe36d297ee46aa9a993382b307fdf1d7
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2021-07-05 13:49:00 +0200

    [FIX] Fix the wheel builder workflow.

--- .github/tools/deployment/upload_anaconda.sh
+++ .github/tools/deployment/upload_anaconda.sh
@@ -2,5 +2,5 @@
 
 conda install -q -y anaconda-client
 
-anaconda -t $CONDA_TOKEN upload --force -u alfaro96 dist/artifact/*
+anaconda -t $ANACONDA_TOKEN upload --force -u alfaro96 dist/artifact/*
 echo "Index: https://pypi.anaconda.org/alfaro96/simple"
