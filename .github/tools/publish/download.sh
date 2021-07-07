0f6bc3ec29862a9c51534eefb6df6398191b3a70
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2021-07-07 12:15:43 +0200

    [FIX] Change library name.

--- .github/tools/publish/download.sh
+++ .github/tools/publish/download.sh
@@ -5,5 +5,5 @@ set -e
 echo "Downloading scikit-lr $SCIKIT_LR_VERSION wheels and source distribution."
 python -m wheelhouse_uploader fetch --version $SCIKIT_LR_VERSION \
                                     --local-folder dist \
-                                    scikit-learn \
+                                    scikit-lr \
                                     https://pypi.anaconda.org/alfaro/simple/scikit-lr
