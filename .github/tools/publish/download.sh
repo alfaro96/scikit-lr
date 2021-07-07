fddf42a76c4b435d5c9a47bed911a2916ea2983d
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2021-07-07 12:15:07 +0200

    [FIX] Change environment variable name.

--- .github/tools/publish/download.sh
+++ .github/tools/publish/download.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Downloading scikit-lr $SCIKIT_LR_VERSION wheels and source distribution."
-python -m wheelhouse_uploader fetch --version $SKLEARN_VERSION \
+python -m wheelhouse_uploader fetch --version $SCIKIT_LR_VERSION \
                                     --local-folder dist \
                                     scikit-learn \
                                     https://pypi.anaconda.org/alfaro/simple/scikit-lr
