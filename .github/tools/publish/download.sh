afdfa3071e383aad5269a3ced60ff525f6319c9b
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2021-07-07 12:22:22 +0200

    [FIX] Change Anaconda repository URL.

--- .github/tools/publish/download.sh
+++ .github/tools/publish/download.sh
@@ -6,4 +6,4 @@ echo "Downloading scikit-lr $SCIKIT_LR_VERSION wheels and source distribution."
 python -m wheelhouse_uploader fetch --version $SCIKIT_LR_VERSION \
                                     --local-folder dist \
                                     scikit-lr \
-                                    https://pypi.anaconda.org/alfaro/simple/scikit-lr
+                                    https://pypi.anaconda.org/alfaro96/simple/scikit-lr
